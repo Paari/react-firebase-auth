5ab5905905edb21e9539cde7edc1bd5e44e3fe16
Paari <0p0a0r0i@gmail.com>
2017-09-04 23:21:59 +0530

    Main authentication file which will handle successful routes

---
+++ src/Auth/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    // {...rest}
+    render={props => (
+    rest.value && rest.user ? (
+      <Component {...props} {...rest} />
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location },
+      }}
+      />
+    )
+  )}
+  />
+);
+
+export default PrivateRoute;
